961331245d09597f5b292fb2a354410ff33ed121
himel1625 <himelmia1625@gmail.com>
2024-09-23 13:51:15 +0600

    implement  Header section

--- script/app.js
+++ script/app.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const menu = document.getElementById('menu');
+const menuItem = document.getElementById('menu-item');
+
+menu.addEventListener('click', function () {
+  menuItem.classList.toggle('hidden');
+});
